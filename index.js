d1d2c1dd4ead08ca860b38d471978954e389c203
Jyoti <jyotigupta282002@mail.com>
2022-05-31 09:49:12 +0530

    heroku applied

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", async (req, res) => {
   return res.render("index");
 });
 //let sheetId = "1hS64YBsANmCnCS0ebhUPuhwmUap755dZXLs8orcaVho";
-let sheetId = "1teS758X1OgRCoHEHNWcZE_77XU5flHoJyCT5ikyeQ80";
+let sheetId = "1Wl6rWlAnG9i1dhiTMPqHMSokgbYOm4O_iWhmoRSOR4s";
 
 app.get("/get-data", async (req, res) => {
   try {
